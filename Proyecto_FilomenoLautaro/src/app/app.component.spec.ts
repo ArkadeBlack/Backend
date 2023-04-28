a99fde2210bc0053ebb852910c068ddf01dc0118
Lautaro FM <lautaroinformatic@gmail.com>
2023-04-27 23:26:46 -0300

    Agregamos NodeJs al proyecto

--- Proyecto_FilomenoLautaro/src/app/app.component.spec.ts
+++ Proyecto_FilomenoLautaro/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Proyecto_Filomeno_Lautaro'`, () => {
+  it(`should have as title 'Proyecto_FilomenoLautaro'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Proyecto_Filomeno_Lautaro');
+    expect(app.title).toEqual('Proyecto_FilomenoLautaro');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Proyecto_Filomeno_Lautaro app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Proyecto_FilomenoLautaro app is running!');
   });
 });
--- Proyecto_FilomenoLautaro/src/app/app.component.ts
+++ Proyecto_FilomenoLautaro/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Proyecto_Filomeno_Lautaro';
+  title = 'Proyecto_FilomenoLautaro';
 }
